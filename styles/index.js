e2312d74f12d903af5fe8ec8bc035efa33955897
deztidev <martinianosanchi@gmail.com>
2021-06-25 16:02:24 -0300

    home and menu finished

--- styles/index.js
+++ styles/index.js
@@ -2,21 +2,30 @@ const burger = document.querySelector(".burger");
 const firstLine = document.querySelector(".burger__line");
 const menu = document.querySelector(".menu");
 const home = document.querySelector(".home");
+const head = document.querySelector(".head");
 
 let count = 0;
-
-console.log(firstLine);
 
 const burgerActivated = () => {
   count += 1;
   firstLine.classList.add("line1");
-  menu.style.display = "block";
-  home.style.display = "none";
+  show(menu);
+  hide(home);
   if (count % 2 == 0) {
     firstLine.classList.remove("line1");
-    menu.style.display = "none";
-    home.style.display = "flex";
+    hide(menu);
+    show(home);
   }
+};
+
+const hide = element => {
+  element.classList.add("hidden");
+  element.classList.remove("active");
+};
+
+const show = element => {
+  element.classList.remove("hidden");
+  element.classList.add("active");
 };
 
 burger.addEventListener("click", burgerActivated);
