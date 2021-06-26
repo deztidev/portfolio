fa2c0c489540408218575ed31c454ef43b7cb67a
deztidev <martinianosanchi@gmail.com>
2021-06-26 15:47:45 -0300

    about

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const burger = document.querySelector(".burger");
 const firstLine = document.querySelector(".burger__line");
 const menu = document.querySelector(".menu");
 const home = document.querySelector(".home");
-const head = document.querySelector(".head");
+const about = document.querySelector(".about");
+const aboutLink = document.querySelectorAll(".list__item")[1];
+const redux = document.querySelector(".redux");
 
 let count = 0;
 
@@ -27,5 +29,17 @@ const show = element => {
   element.classList.remove("hidden");
   element.classList.add("active");
 };
+
+aboutLink.addEventListener("click", function () {
+  show(about);
+});
+
+redux.addEventListener("mouseover", function () {
+  redux.setAttribute("src", "./assets/images/redux-color.svg");
+});
+
+redux.addEventListener("mouseout", function () {
+  redux.setAttribute("src", "./assets/images/redux.png");
+});
 
 burger.addEventListener("click", burgerActivated);
