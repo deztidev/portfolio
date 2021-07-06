c39fe571333def092f006546ec3f967085fc1a33
deztidev <martinianosanchi@gmail.com>
2021-07-06 11:03:59 -0300

    more height changes

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ const router = route => {
             document.querySelector(".contact").style.height =
               (window.innerHeight * 86) / 100 + "px";
             document.querySelector(".contact__container").style.height =
-              (window.innerHeight * 93.7) / 100 + "px";
+              (window.innerHeight * 80.5) / 100 + "px";
             document.querySelector(".form").style.height =
               (window.innerHeight * 43) / 100 + "px";
           });
